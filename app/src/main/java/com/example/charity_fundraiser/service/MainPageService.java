3b1db12682d863bdcce33723c89e11cb1d962c11
Szymon Kaszura <sajmonpele@gmail.com>
2021-03-28 18:16:11 +0200

    Added main page service

--- app/src/main/java/com/example/charity_fundraiser/service/MainPageService.java
+++ app/src/main/java/com/example/charity_fundraiser/service/MainPageService.java
@@ -1,86 +1,19 @@
 package com.example.charity_fundraiser.service;
 
-import android.app.Service;
-import android.content.Intent;
-import android.os.Handler;
-import android.os.HandlerThread;
-import android.os.IBinder;
-import android.os.Looper;
-import android.os.Message;
-import android.widget.Toast;
-import android.os.Process;
 
-import androidx.annotation.Nullable;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class MainPageService extends Service {
-    private Looper serviceLooper;
-    private ServiceHandler serviceHandler;
-    public MainPageService() {
-    }
-    private final class ServiceHandler extends Handler {
-        public ServiceHandler (Looper looper) {
-            super(looper);
-        }
+public class MainPageService {
 
-        @Override
-        public void handleMessage(Message msg) {
-            // Normally we would do some work here, like download a file.
-            // For our sample, we just sleep for 5 seconds.
-            try {
-                // replace this with actually getting code from database later
-                getFundraiserData();
-            } catch (Exception e) {
-                // Restore interrupt status.
-                Thread.currentThread().interrupt();
-            }
-            // Stop the service using the startId, so that we don't stop
-            // the service in the middle of handling another job
-            stopSelf(msg.arg1);
-        }
+    public JSONArray getFundraiserData () throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("title", "Fundraiser for Bob");
+        jsonObject.put("final_amount","30000");
+        JSONArray jsonArray = new JSONArray();
+        jsonArray.put(jsonObject);
+        return jsonArray;
     }
 
-    @Override
-    public void onCreate() {
-        // Start up the thread running the service. Note that we create a
-        // separate thread because the service normally runs in the process's
-        // main thread, which we don't want to block. We also make it
-        // background priority so CPU-intensive work doesn't disrupt our UI.
-        HandlerThread thread = new HandlerThread("ServiceStartArguments",
-                Process.THREAD_PRIORITY_BACKGROUND);
-        thread.start();
-
-        // Get the HandlerThread's Looper and use it for our Handler
-        serviceLooper = thread.getLooper();
-        serviceHandler = new ServiceHandler(serviceLooper);
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
-
-        // For each start request, send a message to start a job and deliver the
-        // start ID so we know which request we're stopping when we finish the job
-        Message msg = serviceHandler.obtainMessage();
-        msg.arg1 = startId;
-        serviceHandler.sendMessage(msg);
-
-        // If we get killed, after returning from here, restart
-        return START_STICKY;
-    }
-
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        // We don't provide binding, so return null
-        return null;
-    }
-
-    @Override
-    public void onDestroy() {
-        Toast.makeText(this, "service done", Toast.LENGTH_SHORT).show();
-    }
-
-    public String getFundraiserData () {
-        return "{}";
-    }
 }

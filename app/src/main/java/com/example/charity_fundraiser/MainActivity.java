c967fc1673cac6c79c928a9c4049e105402c0005
PatrykM-IT <patrykmroz013@gmail.com>
2021-03-29 13:02:06 +0200

    Footer

--- app/src/main/java/com/example/charity_fundraiser/MainActivity.java
+++ app/src/main/java/com/example/charity_fundraiser/MainActivity.java
@@ -1,34 +1,47 @@
 package com.example.charity_fundraiser;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
+import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
-import androidx.drawerlayout.widget.DrawerLayout;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
 
+    //Footer
+    ListView listView;
+    String[] country = new String[] {
+    };
+    List<String> LISTSTRING;
+    LayoutInflater layoutInflater;
+    //Footer
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -51,6 +64,19 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+    //Footer
+        listView = findViewById(R.id.listView);
+        LISTSTRING = new ArrayList<>(Arrays.asList(country));
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, LISTSTRING);
+
+        layoutInflater = getLayoutInflater();
+        ViewGroup footer = (ViewGroup) layoutInflater.inflate(R.layout.listview_footer, listView, false);
+        listView.addFooterView(footer);
+        listView.setAdapter(adapter);
+    //Footer
     }
 
     @Override
